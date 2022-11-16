db8ab948a62696f8b78c6fea5f98703aa160beca
Mike Saunders <94921179+mikegsaunders@users.noreply.github.com>
2022-11-16 12:30:48 +0000

    first commit

--- mapExtract.py
+++ mapExtract.py
@@ -1,10 +1,15 @@
+import os
 from bs4 import BeautifulSoup
 import requests
+
+# SECRETS #
+apiKey = os.environ['API_KEY_SAND']
+# apiKey = os.environ['API_KEY_PROD']
 
 # SOURCE FILES #
 bibList = open("bibList.txt").read().splitlines()
 holdList = open("holdList.txt").read().splitlines()
-apiKey = '12345xert'
+
 
 for bibID, holdID in zip(bibList, holdList):
     url = f'https://api-eu.hosted.exlibrisgroup.com/almaws/v1/bibs/{bibID}?view=full&expand=None&' \
